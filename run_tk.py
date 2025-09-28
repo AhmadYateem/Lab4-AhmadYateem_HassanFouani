bd13ae507c44e6783f4961f52576e9ef3fa4dd3c
Hassan Fouani <hassanfouani33@gmail.com>
2025-09-28 21:40:35 +0300

    i deleted something

--- run_tk.py
+++
@@ -1,9 +0,0 @@
-# Run the Tkinter UI (uses the App class defined in submission/part4_database.py)
-import tkinter as tk
-from submission.part4_database import App, init_db
-
-if __name__ == "__main__":
-    init_db()  # ensure DB exists
-    root = tk.Tk()
-    App(root)
-    root.mainloop()
